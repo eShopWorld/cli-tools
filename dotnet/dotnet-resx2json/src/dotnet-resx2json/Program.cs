61cea0e5ebcaa1ed0c89d0d55fc03a0387cb3b7e
djfr <davidferreirarodrigues@gmail.com>
2016-09-06 11:21:20 +0100

    Initial add to the repo for the resx2json dotnet CLI extension.

---
+++ dotnet/dotnet-resx2json/src/dotnet-resx2json/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Esw.DotNetCli.Resx2Json
+{
+    using JetBrains.Annotations;
+
+    public class Program
+    {
+        public static void Main([NotNull] string[] args)
+        {
+        }
+    }
+}
---
+++ dotnet/dotnet-resx2json/src/dotnet-resx2json/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("dotnet_resx2json")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("885f83ea-5efa-4d2a-ba29-b1505944f379")]
