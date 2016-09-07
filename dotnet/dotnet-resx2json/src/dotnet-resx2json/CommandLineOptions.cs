73b0f3eb1e1fad3fa7d4cbadb1be46faaaad38f1
djfr <davidferreirarodrigues@gmail.com>
2016-09-07 11:00:31 +0100

    Added functions to parse and evaluate some of the input arguments.

---
+++ dotnet/dotnet-resx2json/src/dotnet-resx2json/CommandLineOptions.cs
@@ -0,0 +1,62 @@
+﻿namespace Esw.DotNetCli.Resx2Json
+{
+    using System.Collections.Generic;
+    using System.Reflection;
+    using Microsoft.Extensions.CommandLineUtils;
+
+    public class CommandLineOptions
+    {
+        public string ResxProject { get; set; }
+        public string JsonProject { get; set; }
+        public IList<string> RemainingArguments { get; set; }
+        public bool IsVerbose { get; set; }
+        public bool IsHelp { get; set; }
+
+        public static CommandLineOptions Parse(string[] args)
+        {
+            var app = new CommandLineApplication(throwOnUnexpectedArg: false)
+            {
+                Name = "dotnet resx2json",
+                FullName = "eShopWorld .NET Core CLI Commands"
+            };
+
+            var options = new CommandLineOptions();
+
+            options.Configure(app);
+
+            app.Execute(args);
+            options.IsHelp = app.IsShowingInformation;
+
+            return options;
+        }
+
+        private void Configure(CommandLineApplication app)
+        {
+            var resxProject = app.Option(
+                "-s|--resx-project <project>",
+                "The project to target (defaults to the project in the current directory). Can be a path to a project.json or a project directory.");
+
+            var jsonProject = app.Option(
+                "-o|--json-project <project>",
+                "The path to the project containing Startup (defaults to the target project). Can be a path to a project.json or a project directory.");
+
+            var verbose = app.Option("--verbose", "Show verbose output");
+
+            app.VersionOption("--version", () => AssemblyVersion);
+            app.HelpOption("-?|--help");
+
+            app.OnExecute(() =>
+            {
+                ResxProject = resxProject.Value();
+                JsonProject = jsonProject.Value();
+                IsVerbose = verbose.HasValue();
+                RemainingArguments = app.RemainingArguments;
+            });
+        }
+
+        private static readonly Assembly ThisAssembly = typeof(CommandLineOptions).GetTypeInfo().Assembly;
+
+        private static readonly string AssemblyVersion = ThisAssembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion
+                                                         ?? ThisAssembly.GetName().Version.ToString();
+    }
+}
--- dotnet/dotnet-resx2json/src/dotnet-resx2json/Program.cs
+++ dotnet/dotnet-resx2json/src/dotnet-resx2json/Program.cs
@@ -1,11 +1,47 @@
 ﻿namespace Esw.DotNetCli.Resx2Json
 {
+    using System;
     using JetBrains.Annotations;
+    using Microsoft.DotNet.Cli.Utils;
 
     public class Program
     {
-        public static void Main([NotNull] string[] args)
+        public static int Main([NotNull] string[] args)
         {
+            DebugHelper.HandleDebugSwitch(ref args);
+
+            try
+            {
+                var options = CommandLineOptions.Parse(args);
+
+                HandleVerboseContext(options);
+
+                if (options.IsHelp)
+                {
+                    return 2;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Reporter.Error.WriteLine(ex.Message.Bold().Red());
+                return 1;
+            }
+
+            return 0;
+        }
+
+        private static void HandleVerboseContext(CommandLineOptions options)
+        {
+            bool isVerbose;
+            bool.TryParse(Environment.GetEnvironmentVariable(CommandContext.Variables.Verbose), out isVerbose);
+
+            options.IsVerbose = options.IsVerbose || isVerbose;
+
+            if (options.IsVerbose)
+            {
+                Environment.SetEnvironmentVariable(CommandContext.Variables.Verbose, bool.TrueString);
+            }
         }
     }
 }
