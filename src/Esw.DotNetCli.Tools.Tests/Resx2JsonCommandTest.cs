dc42be85fa66edc7a5961de91794b6e615d69266
djfr <davidferreirarodrigues@gmail.com>
2016-09-30 15:49:53 +0100

    Added the ability to merge resx during the transformation pass and added a few unit tests for the new code.

--- src/Esw.DotNetCli.Tools.Tests/Resx2JsonCommandTest.cs
+++ src/Esw.DotNetCli.Tools.Tests/Resx2JsonCommandTest.cs
@@ -1,14 +1,33 @@
 ﻿using System.Collections.Generic;
 using System.IO;
+using System.Net.Http.Headers;
 using System.Reflection;
+using System.Xml.Linq;
 using Esw.DotNetCli.Tools;
 using Esw.DotNetCli.Tools.Tests.data;
+using FluentAssertions;
 using Newtonsoft.Json;
 using Xunit;
 
 // ReSharper disable once CheckNamespace
 public class Resx2JsonCommandTest
 {
+    public class Run
+    {
+        [Fact, Trait("Category", "Integration")]
+        public void TheOneTestToRuleThemAll()
+        {
+            // #1 clean up before the test
+
+            // #2 run the command to export the json
+
+            // #3 ensure folder structure
+
+            // #4 inspect all the json and compare it to the resx
+            // #4.1 use roslyn to emit the assembly with the resources on the integration project
+        }
+    }
+
     public class ConvertResx2Json
     {
         [Fact, Trait("Category", "Integration")]
@@ -27,19 +46,252 @@ public class Resx2JsonCommandTest
         }
     }
 
-    public class Run
+    public class GetMergedResource
     {
-        [Fact, Trait("Category", "Integration")]
-        public void TheOneTestToRuleThemAll()
+        
+    }
+
+    public class MergeResx
+    {
+        [Fact, Trait("Category", "Unit")]
+        public void Test_SourceWithMore_ThanTarget()
         {
-            // #1 clean up before the test
+            const string source = @"<?xml version=""1.0"" encoding=""utf-8""?>
+<root>
+  <resheader name=""resmimetype"">
+    <value>text/microsoft-resx</value>
+  </resheader>
+  <resheader name=""version"">
+    <value>2.0</value>
+  </resheader>
+  <resheader name=""reader"">
+    <value>System.Resources.ResXResourceReader, System.Windows.Forms, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089</value>
+  </resheader>
+  <resheader name=""writer"">
+    <value>System.Resources.ResXResourceWriter, System.Windows.Forms, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089</value>
+  </resheader>
+  <data name=""CommonResource_One"" xml:space=""preserve"">
+    <value>Default_One</value>
+  </data>
+  <data name=""CommonResource_Three"" xml:space=""preserve"">
+    <value>Default_Three</value>
+  </data>
+  <data name=""CommonResource_Two"" xml:space=""preserve"">
+    <value>Default_Two</value>
+  </data>
+</root>
+";
 
-            // #2 run the command to export the json
+            const string target = @"<?xml version=""1.0"" encoding=""utf-8""?>
+<root>
+  <resheader name=""resmimetype"">
+    <value>text/microsoft-resx</value>
+  </resheader>
+  <resheader name=""version"">
+    <value>2.0</value>
+  </resheader>
+  <resheader name=""reader"">
+    <value>System.Resources.ResXResourceReader, System.Windows.Forms, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089</value>
+  </resheader>
+  <resheader name=""writer"">
+    <value>System.Resources.ResXResourceWriter, System.Windows.Forms, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089</value>
+  </resheader>
+  <data name=""CommonResource_Three"" xml:space=""preserve"">
+    <value>Level2_Three</value>
+  </data>
+</root>
+";
 
-            // #3 ensure folder structure
+            var result = new Resx2JsonCommand("", "").MergeResx(source, target);
 
-            // #4 inspect all the json and compare it to the resx
-            // #4.1 use roslyn to emit the assembly with the resources on the integration project
+            // because we are calling XElement.ToString() there is no <xml> spec ceremony
+            // so it needs to start at the root element and end without trivia
+            result.Should().Be(@"<root>
+  <resheader name=""resmimetype"">
+    <value>text/microsoft-resx</value>
+  </resheader>
+  <resheader name=""version"">
+    <value>2.0</value>
+  </resheader>
+  <resheader name=""reader"">
+    <value>System.Resources.ResXResourceReader, System.Windows.Forms, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089</value>
+  </resheader>
+  <resheader name=""writer"">
+    <value>System.Resources.ResXResourceWriter, System.Windows.Forms, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089</value>
+  </resheader>
+  <data name=""CommonResource_Three"" xml:space=""preserve"">
+    <value>Level2_Three</value>
+  </data>
+  <data name=""CommonResource_One"" xml:space=""preserve"">
+    <value>Default_One</value>
+  </data>
+  <data name=""CommonResource_Two"" xml:space=""preserve"">
+    <value>Default_Two</value>
+  </data>
+</root>"
+                  );
+        }
+
+        [Fact, Trait("Category", "Unit")]
+        public void Test_SourceWithLess_ThanTarget()
+        {
+            const string source = @"<?xml version=""1.0"" encoding=""utf-8""?>
+<root>
+  <resheader name=""resmimetype"">
+    <value>text/microsoft-resx</value>
+  </resheader>
+  <resheader name=""version"">
+    <value>2.0</value>
+  </resheader>
+  <resheader name=""reader"">
+    <value>System.Resources.ResXResourceReader, System.Windows.Forms, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089</value>
+  </resheader>
+  <resheader name=""writer"">
+    <value>System.Resources.ResXResourceWriter, System.Windows.Forms, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089</value>
+  </resheader>
+  <data name=""CommonResource_Three"" xml:space=""preserve"">
+    <value>Level2_Three</value>
+  </data>
+</root>
+";
+
+            const string target = @"<?xml version=""1.0"" encoding=""utf-8""?>
+<root>
+  <resheader name=""resmimetype"">
+    <value>text/microsoft-resx</value>
+  </resheader>
+  <resheader name=""version"">
+    <value>2.0</value>
+  </resheader>
+  <resheader name=""reader"">
+    <value>System.Resources.ResXResourceReader, System.Windows.Forms, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089</value>
+  </resheader>
+  <resheader name=""writer"">
+    <value>System.Resources.ResXResourceWriter, System.Windows.Forms, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089</value>
+  </resheader>
+  <data name=""CommonResource_One"" xml:space=""preserve"">
+    <value>Default_One</value>
+  </data>
+  <data name=""CommonResource_Three"" xml:space=""preserve"">
+    <value>Default_Three</value>
+  </data>
+  <data name=""CommonResource_Two"" xml:space=""preserve"">
+    <value>Default_Two</value>
+  </data>
+</root>
+";
+
+            var result = new Resx2JsonCommand("", "").MergeResx(source, target);
+
+            // because we are calling XElement.ToString() there is no <xml> spec ceremony
+            // so it needs to start at the root element and end without trivia
+            result.Should().Be(@"<root>
+  <resheader name=""resmimetype"">
+    <value>text/microsoft-resx</value>
+  </resheader>
+  <resheader name=""version"">
+    <value>2.0</value>
+  </resheader>
+  <resheader name=""reader"">
+    <value>System.Resources.ResXResourceReader, System.Windows.Forms, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089</value>
+  </resheader>
+  <resheader name=""writer"">
+    <value>System.Resources.ResXResourceWriter, System.Windows.Forms, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089</value>
+  </resheader>
+  <data name=""CommonResource_One"" xml:space=""preserve"">
+    <value>Default_One</value>
+  </data>
+  <data name=""CommonResource_Three"" xml:space=""preserve"">
+    <value>Default_Three</value>
+  </data>
+  <data name=""CommonResource_Two"" xml:space=""preserve"">
+    <value>Default_Two</value>
+  </data>
+</root>"
+                  );
+        }
+    }
+
+    public class ResxDataComparer
+    {
+        public class EqualsImp
+        {
+            [Fact, Trait("Category", "Unit")]
+            public void Test_WithDifferentNames()
+            {
+                var element1 = new XElement("data", new XAttribute("name", "1"));
+                var element2 = new XElement("data", new XAttribute("name", "2"));
+                var result = new Resx2JsonCommand.ResxDataComparer().Equals(element1, element2);
+
+                result.Should().BeFalse();
+            }
+
+            [Fact, Trait("Category", "Unit")]
+            public void Test_WithSameNames()
+            {
+                const string nameValue = "the same name";
+                var element1 = new XElement("data", new XAttribute("name", nameValue));
+                var element2 = new XElement("data", new XAttribute("name", nameValue));
+                var result = new Resx2JsonCommand.ResxDataComparer().Equals(element1, element2);
+
+                result.Should().BeTrue();
+            }
+
+            [Fact, Trait("Category", "Unit")]
+            public void Test_WithTwoNulls()
+            {
+                var result = new Resx2JsonCommand.ResxDataComparer().Equals(null, null);
+
+                result.Should().BeTrue();
+            }
+
+            [Fact, Trait("Category", "Unit")]
+            public void Test_WithFirstNull()
+            {
+                var result = new Resx2JsonCommand.ResxDataComparer().Equals(null, new XElement("something"));
+
+                result.Should().BeFalse();
+            }
+
+            [Fact, Trait("Category", "Unit")]
+            public void Test_WithSecondNull()
+            {
+                var result = new Resx2JsonCommand.ResxDataComparer().Equals(new XElement("something"), null);
+
+                result.Should().BeFalse();
+            }
+        }
+
+        public class GetHashCodeImp
+        {
+            [Fact, Trait("Category", "Unit")]
+            public void Test_WithProperXElement()
+            {
+                const string nameValue = "some name";
+                var element = new XElement("data", new XAttribute("name", nameValue));
+
+                var result = new Resx2JsonCommand.ResxDataComparer().GetHashCode(element);
+
+                result.Should().Be(nameValue.GetHashCode());
+            }
+
+            [Fact, Trait("Category", "Unit")]
+            public void Test_WithNull()
+            {
+                var result = new Resx2JsonCommand.ResxDataComparer().GetHashCode(null);
+
+                result.Should().Be(0);
+            }
+
+            [Fact, Trait("Category", "Unit")]
+            public void Test_WithElementWithoutName()
+            {
+                var element = new XElement("data");
+
+                var result = new Resx2JsonCommand.ResxDataComparer().GetHashCode(element);
+
+                result.Should().Be(0);
+            }
         }
     }
 }
--- src/dotnet-esw/PathHelper.cs
+++ src/dotnet-esw/PathHelper.cs
@@ -3,6 +3,7 @@
     using System;
     using System.IO;
     using System.Linq;
+    using System.Runtime.CompilerServices;
     using System.Text.RegularExpressions;
     using JetBrains.Annotations;
 
@@ -66,27 +67,11 @@
             return absoluteFolder;
         }
 
-        /// <summary>Creates all directories and subdirectories in the specified path unless they already exist.</summary>
-        /// <returns>An object that represents the directory at the specified path. This object is returned regardless of whether a directory at the specified path already exists.</returns>
+        /// <summary>
+        /// Creates all directories and subdirectories in the specified path unless they already exist.
+        /// </summary>
         /// <param name="path">The directory to create. </param>
-        /// <exception cref="T:System.IO.IOException">The directory specified by <paramref name="path" /> is a file.-or-The network name is not known.</exception>
-        /// <exception cref="T:System.UnauthorizedAccessException">The caller does not have the required permission. </exception>
-        /// <exception cref="T:System.ArgumentException">
-        /// <paramref name="path" /> is a zero-length string, contains only white space, or contains one or more invalid characters. You can query for invalid characters by using the <see cref="M:System.IO.Path.GetInvalidPathChars" /> method.-or-<paramref name="path" /> is prefixed with, or contains, only a colon character (:).</exception>
-        /// <exception cref="T:System.ArgumentNullException">
-        /// <paramref name="path" /> is null. </exception>
-        /// <exception cref="T:System.IO.PathTooLongException">The specified path, file name, or both exceed the system-defined maximum length. For example, on Windows-based platforms, paths must be less than 248 characters and file names must be less than 260 characters. </exception>
-        /// <exception cref="T:System.IO.DirectoryNotFoundException">The specified path is invalid (for example, it is on an unmapped drive). </exception>
-        /// <exception cref="T:System.NotSupportedException">
-        /// <paramref name="path" /> contains a colon character (:) that is not part of a drive label ("C:\").</exception>
-        /// <filterpriority>1</filterpriority>
-        /// <PermissionSet>
-        ///   <IPermission class="System.Security.Permissions.FileIOPermission, mscorlib, Version=2.0.3600.0, Culture=neutral, PublicKeyToken=b77a5c561934e089" version="1" Unrestricted="true" />
-        /// </PermissionSet>
-        /// <remarks>
-        /// Inverts control over <see cref="Directory"/> for the <see cref="Directory.CreateDirectory(string)"/> method so that
-        /// we can actually test the other blocks in this class.
-        /// </remarks>>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         internal virtual void CreateDirectory([NotNull]string path)
         {
             Directory.CreateDirectory(path);
