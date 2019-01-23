f808ad29dce04c0067a5fb624ff716c6f599a1d5
Vaclav Haken <vaclav@vaclavkabat.net>
2019-01-23 14:11:04 +0000

    clean up module set up

--- src/EShopWorld.Tools/Commands/CoreModule.cs
+++ src/EShopWorld.Tools/Commands/CoreModule.cs
@@ -1,4 +1,5 @@
-﻿using Autofac;
+﻿using System;
+using Autofac;
 using Eshopworld.Core;
 using Eshopworld.DevOps;
 using Eshopworld.Telemetry;
@@ -21,7 +22,7 @@ namespace EShopWorld.Tools.Commands
         protected override void Load(ContainerBuilder builder)
         {
             var atp = new AzureServiceTokenProvider();
-            var token = atp.GetAccessTokenAsync("https://management.core.windows.net/", "3e14278f-8366-4dfd-bcc8-7e4e9d57f2c1").Result;
+            var token = atp.GetAccessTokenAsync("https://management.core.windows.net/").Result;
             var tokenCredentials = new TokenCredentials(token);
 
             var client = RestClient.Configure()
@@ -31,7 +32,7 @@ namespace EShopWorld.Tools.Commands
                 .Build();
 
             builder.RegisterInstance(client);
-            builder.Register(c=> Azure.Authenticate(client, "3e14278f-8366-4dfd-bcc8-7e4e9d57f2c1"));       
+            builder.Register(c=> Azure.Authenticate(client, null));       
             builder.Register(c=> new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(atp.KeyVaultTokenCallback))); //cannot use token from above - different resource     
 
             builder.RegisterInstance(new ApplicationInsightsManagementClient(new TokenCredentials(token)));
